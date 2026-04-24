0abc8ff3eb61ef620caf8a9a94888168c090679d
Julio Bacoli <juliosantanabacoli@gmail.com>
2026-04-23 23:24:55 -0300

    Segurança

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  res.setHeader('Content-Security-Policy', [
+    "default-src 'self'",
+    "script-src 'self' 'unsafe-inline'",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+    "font-src 'self' https://fonts.gstatic.com",
+    "img-src 'self' data:",
+    "connect-src 'self'",
+    "frame-ancestors 'none'",
+  ].join('; '));
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.setHeader('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'dist/portfolio-pessoal/browser')));
 
 app.get('*', (req, res) => {
