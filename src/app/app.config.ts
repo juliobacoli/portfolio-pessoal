a765ee25e6db72c22b88a86b0d963abd193a311a
Julio Bacoli <juliosantanabacoli@gmail.com>
2026-06-01 11:52:11 -0300

    fix(i18n): pré-carrega pt.json via APP_INITIALIZER para evitar flash de chaves

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, APP_INITIALIZER, inject } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
-import { provideTranslateService } from '@ngx-translate/core';
-import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader, TRANSLATE_HTTP_LOADER_CONFIG } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,7 +12,18 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
-    provideTranslateService({ defaultLanguage: 'pt' }),
-    ...provideTranslateHttpLoader({ prefix: '/assets/i18n/', suffix: '.json' })
+    { provide: TRANSLATE_HTTP_LOADER_CONFIG, useValue: { prefix: '/assets/i18n/', suffix: '.json' } },
+    provideTranslateService({
+      defaultLanguage: 'pt',
+      loader: { provide: TranslateLoader, useClass: TranslateHttpLoader }
+    }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        const translate = inject(TranslateService);
+        return () => translate.use('pt');
+      },
+      multi: true
+    }
   ],
 };
